73eaaed794c1f0781a049e8c81fd2c647e1c0a5a
Al Viro <viro@zeniv.linux.org.uk>
2016-12-10 22:25:45 +0300

    fix off-by-one in argument passed by iterate_fd() to callbacks Noticed by Pavel Roskin; the thing in his patch I disagree with was compensating for that shite in callbacks instead of fixing it once in the iterator itself.
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- fs/file.c
+++ fs/file.c
@@ -698,28 +698,6 @@ void do_close_on_exec(struct files_struct *files)
 	spin_unlock(&files->file_lock);
 }
 
-int iterate_fd(struct files_struct *files, unsigned n,
-		int (*f)(const void *, struct file *, unsigned),
-		const void *p)
-{
-	struct fdtable *fdt;
-	struct file *file;
-	int res = 0;
-	if (!files)
-		return 0;
-	spin_lock(&files->file_lock);
-	fdt = files_fdtable(files);
-	while (!res && n < fdt->max_fds) {
-		file = rcu_dereference_check_fdtable(files, fdt->fd[n++]);
-		if (file)
-			res = f(p, file, n);
-	}
-	spin_unlock(&files->file_lock);
-	return res;
-}
-EXPORT_SYMBOL(iterate_fd);
-
-
 struct file *fget(unsigned int fd)
 {
 	struct file *file;
@@ -989,3 +967,26 @@ int f_dupfd(unsigned int from, struct file *file, unsigned flags)
 	}
 	return err;
 }
+
+int iterate_fd(struct files_struct *files, unsigned n,
+		int (*f)(const void *, struct file *, unsigned),
+		const void *p)
+{
+	struct fdtable *fdt;
+	int res = 0;
+	if (!files)
+		return 0;
+	spin_lock(&files->file_lock);
+	for (fdt = files_fdtable(files); n < fdt->max_fds; n++) {
+		struct file *file;
+		file = rcu_dereference_check_fdtable(files, fdt->fd[n]);
+		if (!file)
+			continue;
+		res = f(p, file, n);
+		if (res)
+			break;
+	}
+	spin_unlock(&files->file_lock);
+	return res;
+}
+EXPORT_SYMBOL(iterate_fd);
